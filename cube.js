4d373d145a15c5067b698fd517647ebf65852db2
Matvey Boguslavskiy <hi@matvey.contact>
2023-07-14 21:22:11 +0100

    three.js test

---
+++ cube.js
@@ -0,0 +1,31 @@
+// Initialize scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(380, 300);
+renderer.setClearColor(0xffffff);
+document.getElementById('canvas-container').appendChild(renderer.domElement);
+
+// Create geometry and material for the cube
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: 0x000000 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Position the camera  
+camera.position.z = 5;
+
+// Animation loop
+function animate() {
+  requestAnimationFrame(animate);
+
+  // Rotate the cube
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  // Render the scene
+  renderer.render(scene, camera);
+}
+
+// Start the animation loop
+animate();
